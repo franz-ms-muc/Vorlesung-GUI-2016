e57b6740ca7c54889a7091a42d5a45555c50176c
Johannes Weigend <johannes.weigend@qaware.de>
2016-03-18 14:35:47 +0100

    U1.

---
+++ codebase/u1-search/src/de/hsro/gui/u1/SearchApplication.java
@@ -0,0 +1,40 @@
+/*
+ * _____________________________________________________________________________
+ * 
+ *           Project:    GUI 2015
+ * _____________________________________________________________________________
+ * 
+ *        Created by:    Johannes Weigend, QAware GmbH
+ *     Creation date:    March - July 2015
+ * _____________________________________________________________________________
+ * 
+ *         Copyright:    Hochschule Rosenheim
+ * _____________________________________________________________________________ 
+ */
+package de.hsro.gui.u1;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * SearchApplication
+ * @author weigend
+ */
+public class SearchApplication extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SearchApplication.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {     
+        Scene scene = new Scene(new SearchView());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+}
---
+++ codebase/u1-search/src/de/hsro/gui/u1/SearchView.java
@@ -0,0 +1,42 @@
+/*
+ * _____________________________________________________________________________
+ * 
+ *           Project:    GUI 2015
+ * _____________________________________________________________________________
+ * 
+ *        Created by:    Johannes Weigend, QAware GmbH
+ *     Creation date:    March - July 2015
+ * _____________________________________________________________________________
+ * 
+ *         Copyright:    Hochschule Rosenheim
+ * _____________________________________________________________________________ 
+ */
+package de.hsro.gui.u1;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+/**
+ * Simple View which shows two buttons.
+ * @author weigend
+ */
+public class SearchView extends HBox {
+
+    private final Button searchButton;
+    private final TextField searchText;
+
+    public SearchView() {
+
+        searchText = new TextField();
+        searchText.setPromptText("Enter a Expression");
+       
+        searchButton = new Button("Search");     
+        searchButton.disableProperty().bind(
+                searchText.textProperty().isEmpty()
+        );
+       
+        getChildren().addAll(searchText, searchButton);
+    }
+
+}
