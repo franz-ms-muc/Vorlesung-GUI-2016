fe680297f0c3fb4b525c88c57a9bc8e9d3c2217d
Johannes Weigend <johannes.weigend@qaware.de>
2016-03-21 09:22:43 +0100

    U1.

--- codebase/u1-search2/src/de/hsro/u1/search2/SearchViewController.java
+++ codebase/u1-search2/src/de/hsro/u1/search2/SearchViewController.java
@@ -16,28 +16,21 @@ package de.hsro.u1.search2;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
  *
  * @author weigend
  */
 public class SearchViewController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+        
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }   
+    
+    public void onSearchButtonClicked(ActionEvent e) {
+        System.out.println(e);
+    }
     
 }
