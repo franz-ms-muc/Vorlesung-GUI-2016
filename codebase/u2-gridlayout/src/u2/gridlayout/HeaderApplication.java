7cf34c49e5b990eee694a918a691ac61585aec23
Johannes Weigend <johannes.weigend@qaware.de>
2016-04-04 20:09:11 +0200

    U2.

---
+++ codebase/u2-gridlayout/src/u2/gridlayout/HeaderApplication.java
@@ -0,0 +1,45 @@
+/*
+ _____________________________________________________________________________
+ 
+            Project:    GUI2016
+  _____________________________________________________________________________
+  
+         Created by:    Johannes Weigend, QAware GmbH
+      Creation date:    April 2016
+  _____________________________________________________________________________
+  
+          License:      Apache License 2.0
+  _____________________________________________________________________________ 
+ */
+package u2.gridlayout;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Header Main.
+ * @author weigend
+ */
+public class HeaderApplication extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Header.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
