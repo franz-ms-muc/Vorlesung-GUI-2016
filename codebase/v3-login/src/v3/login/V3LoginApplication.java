7ba7365aca7c90fa7a5cf66cb7c4d75ad9a96e2a
Johannes Weigend <johannes.weigend@qaware.de>
2016-04-11 17:01:30 +0200

    Vorlesung 11.04. - Events - Vorlesungsbeispiel.

--- codebase/v3-login/src/v3/login/V3LoginApplication.java
+++ codebase/v3-login/src/v3/login/V3LoginApplication.java
@@ -1,7 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ _____________________________________________________________________________
+ 
+            Project:    GUI 2016
+  _____________________________________________________________________________
+  
+         Created by:    Johannes Weigend, QAware GmbH
+      Creation date:    March 2016
+  _____________________________________________________________________________
+  
+          License:      Apache License 2.0
+  _____________________________________________________________________________ 
  */
 package v3.login;
 
